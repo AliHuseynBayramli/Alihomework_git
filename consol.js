9fd1cc8d05ca8baa71515353fd06feccd40e4a87
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-11-24 17:51:22 +0400

    home work js1

---
+++ consol.js
@@ -0,0 +1,21 @@
+const randomNumber = Math.floor(Math.random() * (100 - 95 + 1)) + 95;
+console.log("random number from 95 to 100 is " + randomNumber);
+
+
+const amountOfBooks = Number(5);
+const booksCost = Number(20);
+
+
+const totalCost = amountOfBooks * booksCost;
+
+
+console.log("Total cost:", totalCost);
+
+let num = 5; 
+
+let power = num ** 5;
+
+let remainder = num % 3;
+
+console.log("Число в 5-й степени:", power);
+console.log("Остаток при делении на 3:", remainder);
