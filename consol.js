88abc18d510101a2e49cfaf5b1d755980a317299
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-11-27 18:38:48 +0400

    js3 homework

--- consol.js
+++ consol.js
@@ -1,21 +1,11 @@
-const randomNumber = Math.floor(Math.random() * (100 - 95 + 1)) + 95;
-console.log("random number from 95 to 100 is " + randomNumber);
+let name="Alihuseyn";
+let surname="bayramli";
+let fullName=name+" "+surname;
+console.log(fullName.split(" ")[0][0],fullName.split(" ")[1][0])
 
+let name1= prompt("Write your name");
+let surname1=prompt("Write your surname");
+let fullName1=name1+" "+surname1;
+let split_name=fullName1.split(" ")
 
-const amountOfBooks = Number(5);
-const booksCost = Number(20);
-
-
-const totalCost = amountOfBooks * booksCost;
-
-
-console.log("Total cost:", totalCost);
-
-let num = 5; 
-
-let power = num ** 5;
-
-let remainder = num % 3;
-
-console.log("Число в 5-й степени:", power);
-console.log("Остаток при делении на 3:", remainder);
+console.log(split_name[1],split_name[0])
