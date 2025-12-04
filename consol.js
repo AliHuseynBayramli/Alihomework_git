131386586e5d9904966282bcc01ac47156c8bc57
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-12-04 18:30:44 +0400

    js homework 4

--- consol.js
+++ consol.js
@@ -1,18 +1,27 @@
-//first
-let str1 = prompt("write a line:");
-console.log("lenght of the line: " + str1.length);
-//second
-let str2 = prompt("write second line and i coubt e:");
-console.log(str2.includes("e"));
-//tird
-let str3 = prompt("write tird line and ill tel u is there word code in start:");
-console.log(str3.startsWith("code"));
-//fourth
-let str4 = prompt("write fourth line and ill tel u is there word code at the end:");
-console.log(str4.endsWith("code"));
-//fiveth
-let str = prompt("write last line");
-let result = str.replaceAll("t", "####*");
-console.log(result);
+//age tusk
+let age =Number(prompt("how old are you?"))
+if(age>=18){
+    console.log("You are welcam")
+}else {
+    console.log("You are not allow to this sayt")
+}
+//name surname tusk
 
 
+let fullname = prompt("what is your full name")
+
+let parts = fullname.split(" ")
+
+let firstName= parts[0]
+
+console.log("Salam , "+ firstName +" !")
+
+
+//number tusk
+let number = prompt("write a 4-long number")
+let num1 = Number(number[0])
+let num2 = Number(number[1])
+let num3 = Number(number[2])
+let num4 = Number(number[3])
+let sum = num1+num2+num3+num4
+console.log("Sum of every single number is "+ sum)
