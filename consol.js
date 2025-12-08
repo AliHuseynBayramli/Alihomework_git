8983fa3f47bcfa1c44c66765df38504c6b9add14
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-12-08 18:52:13 +0400

    js5 homework

--- consol.js
+++ consol.js
@@ -1,27 +1,49 @@
-//age tusk
-let age =Number(prompt("how old are you?"))
-if(age>=18){
-    console.log("You are welcam")
-}else {
-    console.log("You are not allow to this sayt")
+//first one
+let num = Number(prompt("Введите число от 1 до 12:"));
+let month;
+
+switch (num) {
+    case 1: month = "январь"; break;
+    case 2: month = "февраль"; break;
+    case 3: month = "март"; break;
+    case 4: month = "апрель"; break;
+    case 5: month = "май"; break;
+    case 6: month = "июнь"; break;
+    case 7: month = "июль"; break;
+    case 8: month = "август"; break;
+    case 9: month = "сентябрь"; break;
+    case 10: month = "октябрь"; break;
+    case 11: month = "ноябрь"; break;
+    case 12: month = "декабрь"; break;
+    default: month = "Неверный номер месяца";
 }
-//name surname tusk
 
+console.log(month);
 
-let fullname = prompt("what is your full name")
+//second one
 
-let parts = fullname.split(" ")
+let input = prompt("Веди год месяц и число")
+let parts = input.split(" ");
+let year = parts[0];
+let monthNum = Number(parts[1]);
+let day = parts[2];
 
-let firstName= parts[0]
+let monthName;
 
-console.log("Salam , "+ firstName +" !")
+switch (monthNum) {
+    case 1: monthName = "января"; break;
+    case 2: monthName = "февраля"; break;
+    case 3: monthName = "марта"; break;
+    case 4: monthName = "апреля"; break;
+    case 5: monthName = "мая"; break;
+    case 6: monthName = "июня"; break;
+    case 7: monthName = "июля"; break;
+    case 8: monthName = "августа"; break;
+    case 9: monthName = "сентября"; break;
+    case 10: monthName = "октября"; break;
+    case 11: monthName = "ноября"; break;
+    case 12: monthName = "декабря"; break;
+    default: monthName = "неизвестного месяца";
+}
 
-
-//number tusk
-let number = prompt("write a 4-long number")
-let num1 = Number(number[0])
-let num2 = Number(number[1])
-let num3 = Number(number[2])
-let num4 = Number(number[3])
-let sum = num1+num2+num3+num4
-console.log("Sum of every single number is "+ sum)
+console.log("Сегодня " + day + " " + monthName + " " + year + " года.");
