14a166a7d0917fe8855d3f826cce5d550ef0eabb
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-12-11 19:16:45 +0400

    bug fix

--- consol.js
+++ consol.js
@@ -1,6 +1,14 @@
+function rubl_dollar(dollar){
+   let u = dollar * 75;
+   return u;
+
+}
+
+
+
 let rubl = 100;
 let dollar = 50;
-let ru_dol = dollar*79.25;
+let ru_dol = rubl_dollar(dollar) ;
 console.log("yor rubl wolet has " + rubl)
 console.log("yor dollar wolet has "+ dollar)
 console.log("your bank akkaunt has "+ ru_dol+rubl+ " rubls")
