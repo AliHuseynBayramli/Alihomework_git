e283ecd06e79f249f8d7e0baab76a2fe95b80d8b
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-12-25 18:12:46 +0400

    js10 homework

--- consol.js
+++ consol.js
@@ -1,8 +1,12 @@
-//first task
-let text = prompt("write an text");
-for(let i=0;i<text.length;i++){
-   if(text[i]>="0" && text[i]<="9"){
-      console.log("text has number");
-      break;
-   }
+let arr = [7, 3, 8, 9, 2, 12];
+let newArr = [];
+
+for (let i = 0; i < arrr.length; i++) {
+  if (arrr[i] > 5) {
+    newArr.unshift(arrr[i]);
+  } else {
+    newArr.push(arrr[i]);
+  }
 }
+
+console.log(newArr);
