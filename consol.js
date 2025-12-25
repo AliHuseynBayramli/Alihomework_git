53bcf8b44862bafdfbcb85c92081137f79f8c5e7
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2025-12-25 19:10:13 +0400

    bug

--- consol.js
+++ consol.js
@@ -1,8 +1,8 @@
 let arr = [7, 3, 8, 9, 2, 12];
 let newArr = [];
 
-for (let i = 0; i < arrr.length; i++) {
-  if (arrr[i] > 5) {
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] > 5) {
     newArr.unshift(arr[i]);
   } else {
     newArr.push(arr[i]);
