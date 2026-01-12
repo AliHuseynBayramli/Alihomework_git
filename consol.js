1155f3c765903cf61f528d7e66ec005178311227
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2026-01-12 18:31:06 +0400

    js13 homework

--- consol.js
+++ consol.js
@@ -1,7 +1,22 @@
-let arr = [1, 2, 3, 4, 5];
+let arr = [1, 4, 7, 8, 11, 20, 3];
+let sum = 0;
+arr.forEach(num => {
+  if (num % 2 !== 0) {
+    sum += num;
+  }
+});
+console.log(sum); 
 
-let arrCopy = arr.map(num => num * 2);
+
+//second task 
+
+let words = ["js", "front", "html", "css"];
+
+let reversedWords = words.map(word =>
+  word.split("").reverse().join("")
+);
+
+console.log(reversedWords);
 
 
-console.log(arrCopy);
 
