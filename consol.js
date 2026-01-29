c6c8ce6d20ee65660f6d970b43d35f0521b0b503
Alihuseyn <67820364+AliHuseynBayramli@users.noreply.github.com>
2026-01-29 16:30:00 +0400

    js homework

--- consol.js
+++ consol.js
@@ -1,23 +1,24 @@
-let User = {
-  name: "John",
-  age: 30,
-
-  setAge(newAge) {
-    this.age = newAge;
-  },
-
-  yearsToRetirement() {
-    if (this.age < 65) {
-      return `Years left until retirement: ${65 - this.age}`;
-    } else if (this.age === 65) {
-      return "The user will receive retirement this year.";
-    } else {
-      return "The user is already retired.";
-    }
+// first
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    console.log("Enter");
   }
-};
+});
+// second
+document.getElementById("btn").addEventListener("click", function () {
+  const name = document.getElementById("username").value;
+  const comment = document.getElementById("comment").value;
 
+  console.log("Имя пользователя:", name);
+  console.log("Комментарий:", comment);
+});
+// third
+document.getElementById("loginBtn").addEventListener("click", function () {
+  const password = document.getElementById("password").value;
 
-User.setAge(40);
-console.log(User.yearsToRetirement());
-
+  if (password.length > 5) {
+    console.log("login successful");
+  } else {
+    console.log("login failed");
+  }
+});
